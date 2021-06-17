41d55ee01de0cf6840c4e4ed5eb222bfc87ef2ec
karan <karansin@microsoft.com>
2021-06-16 17:16:00 -0700

    move files up

--- src/tests/end-to-end/tests/common/segfault.ts
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-export default function (): void {
-    const SegfaultHandler = require('segfault-handler');
-    SegfaultHandler.registerHandler('crash.log');
-    console.log('setting up');
-}
